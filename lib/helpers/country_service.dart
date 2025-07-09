94d1c7cfe830bc423103bfdb9fb27cb211f1f8a1
Owis AlHammoud <owisalhammoud@gmail.com>
2025-07-09 15:11:31 +0300

    fix bugs endpoint

--- lib/helpers/country_service.dart
+++ lib/helpers/country_service.dart
@@ -6,7 +6,7 @@ import '../models/country.dart';
 
 class CountryService {
   static Future<List<Country>> fetchCountries() async {
-    final uri = Uri.parse('https://restcountries.com/v3.1/all');
+    final uri = Uri.parse('https://restcountries.com/v3.1/all?fields=name');
     final response = await http.get(uri);
     if (response.statusCode == 200) {
       final List<dynamic> jsonList = jsonDecode(response.body) as List<dynamic>;
